7e95ebd340ecfe7ad8af8acc5710b205fca26478
Cole Collins <ncollins@utexas.edu>
2019-10-04 00:38:00 -0700

    prototype of side nav

--- src/index.js
+++ src/index.js
@@ -1,8 +1,37 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import EmbeddedGist from "./EmbeddedGist.js"
+
+const buttonStyle = {
+    margin: '10px',
+    display: 'block'
+}
+
+const buttonContainerStyle = {
+    float: 'left',
+    position: 'sticky',
+    top: '50px',
+    left: '50px',
+}
+
+const SideBtns = () => {
+    return <div className="container">
+    <div className="row">
+        <Index/>
+    </div>
+</div>
+}
+
 const Ree = () => {
-    return <div className="container"><div className="row"><Index/></div></div>
+
+    return <div>
+        <div style={buttonContainerStyle}>
+            <button type="button" className="btn btn-info" style={buttonStyle}>Javascript</button>
+            <button type="button" className="btn btn-info" style={buttonStyle}>Typescript</button>
+            <button type="button" className="btn btn-info" style={buttonStyle}>Golang</button>
+        </div>
+        <SideBtns/>
+    </div>
 }
 
 
