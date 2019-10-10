ecf89d52523d70a69b2e0e8e5733ddfde3ca9948
Cole Collins <ncollins@utexas.edu>
2019-10-09 20:13:37 -0700

    ran through prettier

--- src/index.js
+++ src/index.js
@@ -1,213 +1,225 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import EmbeddedGist from "./EmbeddedGist.js"
+import EmbeddedGist from "./EmbeddedGist.js";
 
 ///////////////////////////////////////////////////
 
 const buttonStyle = {
-    margin: '10px',
-    display: 'block',
-    width: '100px'
-}
+	margin: "10px",
+	display: "block",
+	width: "100px"
+};
 
 const buttonContainerStyle = {
-    float: 'left',
-    position: 'sticky',
-    top: '90px',
-    left: '90px',
-}
+	float: "left",
+	position: "sticky",
+	top: "90px",
+	left: "90px"
+};
 
 const jsButtonStyle = {
-    background: "#f5de19",
-    color: "black",
-    'border-color': "grey"
-}
+	background: "#f5de19",
+	color: "black",
+	"border-color": "grey"
+};
 
 const tsButtonStyle = {
-    background: "#007acc",
-    color: "white",
-    'border-color': "grey"
-}
+	background: "#007acc",
+	color: "white",
+	"border-color": "grey"
+};
 
 const goButtonStyle = {
-    background: "#67d1df",
-    color: "black",
-    'border-color': "grey"
-}
+	background: "#67d1df",
+	color: "black",
+	"border-color": "grey"
+};
 
 const containterStyle = {
-    // 'padding-top': "2%",
-    'min-height': "90vh",
-    'margin-top': "15px",
-    'margin-bottom': "15px",
-    'background-color': "#004877"
-}
+	// 'padding-top': "2%",
+	"min-height": "90vh",
+	"margin-top": "15px",
+	"margin-bottom": "15px",
+	"background-color": "#004877"
+};
 
 const headerStyle = {
-    'margin-top' : "15px",
-    'margin-bottom' : "15px",
-    'display' : "inline-block",
-    'color' : "white"
-}
+	"margin-top": "15px",
+	"margin-bottom": "15px",
+	display: "inline-block",
+	color: "white"
+};
 
 /////////////////////////////////////////////////////////
 
 class LandingPage extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = { 'language' : "javascript"};
-      this.handleLanguageChange = this.handleLanguageChange.bind(this);
-    }
-  
-    handleLanguageChange(languageClicked) {
-        this.setState({language: languageClicked});
-    }
-  
-    componentDidMount() {
-      
-    }
-  
-    componentWillUnmount() {
-      
-    }
+	constructor(props) {
+		super(props);
+		this.state = { language: "javascript" };
+		this.handleLanguageChange = this.handleLanguageChange.bind(this);
+	}
 
-    render() {
-      return (
-        <div >
-          <LanguageButtons language={this.state.language} onLanguageChange={this.handleLanguageChange}/>
-          <LanguageContent language={this.state.language} />
-        </div>
-      );
-    }
-  }
+	handleLanguageChange(languageClicked) {
+		this.setState({ language: languageClicked });
+	}
+
+	componentDidMount() {}
+
+	componentWillUnmount() {}
+
+	render() {
+		return (
+			<div>
+				<LanguageButtons
+					language={this.state.language}
+					onLanguageChange={this.handleLanguageChange}
+				/>
+				<LanguageContent language={this.state.language} />
+			</div>
+		);
+	}
+}
 
 class LanguageButtons extends React.Component {
-    constructor(props) {
-        super(props);
-        this.handleChange = this.handleChange.bind(this);
-    }
+	constructor(props) {
+		super(props);
+		this.handleChange = this.handleChange.bind(this);
+	}
 
-    handleChange(e) {
-        this.props.onLanguageChange(e);
-    }    
-  
-    componentDidMount() {
-      
-    }
-  
-    componentWillUnmount() {
-      
-    }
+	handleChange(e) {
+		this.props.onLanguageChange(e);
+	}
 
-    render() {
-      return (
-        <div style={buttonContainerStyle}>
-            <button type="button" className="btn btn-info" style={Object.assign({}, buttonStyle, jsButtonStyle)} onClick={() => { this.handleChange('javascript')}}>Javascript</button>
-            <button type="button" className="btn btn-info" style={Object.assign({}, buttonStyle, tsButtonStyle)} onClick={() => { this.handleChange('typescript')}}>Typescript</button>
-            <button type="button" className="btn btn-info" style={Object.assign({}, buttonStyle, goButtonStyle)} onClick={() => { this.handleChange('golang')}}>Golang</button>
-        </div>
-      );
-    }
-  }
+	componentDidMount() {}
 
-  class LanguageContent extends React.Component {
-    constructor(props) {
-        super(props);
-        this.hideGist = this.hideGist.bind(this);
-        this.showGist = this.showGist.bind(this);
-        this.state = { 'hiddenGists' : { 'basicTypes' : true }};
+	componentWillUnmount() {}
 
-    }
-    
-    hideGist(e) {
-        var newState = this.state.hiddenGists
-        newState[e] = false
-        this.setState({hiddenGists: newState});
-    }    
-    
-    showGist(e) {
-        var newState = this.state.hiddenGists
-        newState[e] = true
-        this.setState({hiddenGists: newState});
-    }    
-    
-    componentDidMount() {
-        
-    }
-    
-    componentWillUnmount() {
-        
-    }
-    
-    render() {
+	render() {
+		return (
+			<div style={buttonContainerStyle}>
+				<button
+					type="button"
+					className="btn btn-info"
+					style={Object.assign({}, buttonStyle, jsButtonStyle)}
+					onClick={() => {
+						this.handleChange("javascript");
+					}}
+				>
+					Javascript
+				</button>
+				<button
+					type="button"
+					className="btn btn-info"
+					style={Object.assign({}, buttonStyle, tsButtonStyle)}
+					onClick={() => {
+						this.handleChange("typescript");
+					}}
+				>
+					Typescript
+				</button>
+				<button
+					type="button"
+					className="btn btn-info"
+					style={Object.assign({}, buttonStyle, goButtonStyle)}
+					onClick={() => {
+						this.handleChange("golang");
+					}}
+				>
+					Golang
+				</button>
+			</div>
+		);
+	}
+}
 
-    let x
-    let y
+class LanguageContent extends React.Component {
+	constructor(props) {
+		super(props);
+		this.hideGist = this.hideGist.bind(this);
+		this.showGist = this.showGist.bind(this);
+		this.state = { hiddenGists: { basicTypes: true } };
+	}
 
-      if (this.state.hiddenGists['basicTypes']) {
-            x = <span onClick={() => this.hideGist('basicTypes')}>⬇️</span>
-      } else {
-            x = <span onClick={() => this.showGist('basicTypes')}>⬆️</span>
-        }
+	hideGist(e) {
+		var newState = this.state.hiddenGists;
+		newState[e] = false;
+		this.setState({ hiddenGists: newState });
+	}
 
-        if (this.state.hiddenGists['basicTypes'] === true) {
-            y = <TopicGists topic="basicTypes" language={this.props.language}/>
-      }
+	showGist(e) {
+		var newState = this.state.hiddenGists;
+		newState[e] = true;
+		this.setState({ hiddenGists: newState });
+	}
 
-      return (
-        <div style={containterStyle} className="container">
-            <h1 style={headerStyle}>Basic Types {x}</h1>
-            {y}
-        </div>
-      );
-    }
-  }
+	componentDidMount() {}
+
+	componentWillUnmount() {}
+
+	render() {
+		let x;
+		let y;
+
+		if (this.state.hiddenGists["basicTypes"]) {
+			x = <span onClick={() => this.hideGist("basicTypes")}>⬇️</span>;
+		} else {
+			x = <span onClick={() => this.showGist("basicTypes")}>⬆️</span>;
+		}
+
+		if (this.state.hiddenGists["basicTypes"] === true) {
+			y = <TopicGists topic="basicTypes" language={this.props.language} />;
+		}
+
+		return (
+			<div style={containterStyle} className="container">
+				<h1 style={headerStyle}>Basic Types {x}</h1>
+				{y}
+			</div>
+		);
+	}
+}
 
 /////////////////////////////////////////////////////////
 
-// const LanguageContent = (props) => {
-//     return <div style={containterStyle} className="container">
-//         <h1 style={headerStyle}>Basic Types <span onClick={() => { alert('test')}}>⬇️</span></h1>
-//         <TopicGists topic="basicTypes" language={props.language}/>
-//     </div>
-// }
+const TopicGists = props => {
+	const gists = {
+		javascript: [
+			{
+				gistId: "hippohipporhino/286ec9fd570b151ea25cad8d56cdefb1",
+				fileName: "js_basic_types.js",
+				topic: "basicTypes"
+			}
+		],
+		typescript: [
+			{
+				gistId: "hippohipporhino/286ec9fd570b151ea25cad8d56cdefb1",
+				fileName: "ts_basic_types.ts",
+				topic: "basicTypes"
+			}
+		],
+		golang: [
+			{
+				gistId: "hippohipporhino/286ec9fd570b151ea25cad8d56cdefb1",
+				fileName: "go_basic_types.go",
+				topic: "basicTypes"
+			}
+		]
+	};
 
-const TopicGists = (props) => {
+	var gistElements = gists[props.language].map(gist => {
+		if (gist.topic === props.topic) {
+			return (
+				<EmbeddedGist
+					key={gist.fileName}
+					gist={gist.gistId}
+					file={gist.fileName}
+				></EmbeddedGist>
+			);
+		}
+	});
 
-    const gists = {
-        javascript : [
-            {
-                "gistId" : 'hippohipporhino/286ec9fd570b151ea25cad8d56cdefb1',
-                "fileName" : 'js_basic_types.js',
-                "topic" : 'basicTypes'
-            }
-        ],
-        typescript : [
-            {
-                "gistId" : 'hippohipporhino/286ec9fd570b151ea25cad8d56cdefb1',
-                "fileName" : 'ts_basic_types.ts',
-                "topic" : 'basicTypes'
-            }
-        ],
-        golang : [
-            {
-                "gistId" : 'hippohipporhino/286ec9fd570b151ea25cad8d56cdefb1',
-                "fileName" : 'go_basic_types.go',
-                "topic" : 'basicTypes'
-            }
-        ],
-    }
-
-    var gistElements = gists[props.language].map((gist) => {
-        if (gist.topic === props.topic){
-            return <EmbeddedGist key={gist.fileName} gist={gist.gistId} file={gist.fileName}></EmbeddedGist>
-        }
-    });
-
-    return  <div className="row">
-        {gistElements}    
-    </div>
-}
+	return <div className="row">{gistElements}</div>;
+};
 
 ReactDOM.render(<LandingPage />, document.getElementById("index"));
 
